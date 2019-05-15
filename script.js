fc5ff54a20afdf7a39cabd26a26810a49de51a8a
Ivan <ivandidenkoweb@gmail.com>
2019-05-16 00:56:56 +0300

    ДЗ2

--- script.js
+++ script.js
@@ -21,6 +21,35 @@ let answer = function () {
             answer();
         }
     };
+
+    // Второй способ цикла
+
+    // do {
+    //     let i = 0;
+    //     let a = prompt("Введите обязательную статью расходов в этом месяце"),
+    //         b = prompt("Во сколько обойдется");
+    //     if ((typeof (a) === 'string') && (typeof (a) != null) && (typeof (b) != null) &&
+    //         (a != '') && (b != '') && (a.length < 50)) {
+    //         appData.expenses[a] = b;
+    //     } else {
+    //         answer();
+    //     }
+    //     i++;
+    // } while (i < 2);
+
+    // Третий способ цикла
+    // let i = 0;
+    // while (i < 2){
+    // let a = prompt("Введите обязательную статью расходов в этом месяце"),
+    //         b = prompt("Во сколько обойдется");
+    //     if ((typeof (a) === 'string') && (typeof (a) != null) && (typeof (b) != null) &&
+    //         (a != '') && (b != '') && (a.length < 50)) {
+    //         appData.expenses[a] = b;
+    //     } else {
+    //         answer();
+    //     }
+    //     i++;
+    // };
 };
 
 answer();
