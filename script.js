6d21de2a3f1d3f6eaa3fef2116bb92eb59d87ed6
Ivan <ivandidenkoweb@gmail.com>
2019-05-15 13:36:49 +0300

    Это третий коммит через bash

---
+++ script.js
@@ -0,0 +1,21 @@
+let money = prompt("Ваш бюджет на месяц?");
+let time = prompt("Введите дату в формате YYYY-MM-DD");
+let answer1 = prompt("Введите обязательную статью расходов в этом месяце");
+let answer2 = prompt("Во сколько обойдется");
+
+let expenses = {};
+
+expenses[answer1] = answer2;
+
+let appData = {
+    money: money,
+    timeData: time,
+    expenses: expenses,
+    optionalExpenses: undefined,
+    income: undefined,
+    savings: false
+};
+
+console.log(appData.expenses);
+
+alert(Number(appData.money) / 30);
